6d7a849d9e4b0077001795d943930ed2b5f5211a
Noikstrax <noikstrax@tuta.io>
2025-02-01 14:32:22 +0300

    Add myIncludes.js and myIndexOf *Fix index,html for mySlice *Add myIncludes.js *Add myIndefOf.js

---
+++ tasks/task9/myIncludes/myIncludes.js
@@ -0,0 +1,19 @@
+let array = ['Яблоко', 'Апельсин', 'Яблоко', false, NaN];
+
+function myIncludes(array, item, from = 0) {
+    let range = array.length;
+    if (from < 0) from = array.length + from;
+
+    for (let i = from; i < array.length; i++) {
+        if (isNaN(array[i]) && typeof(item) === 'number') return true;
+        if (array[i] === item) return true;
+    }
+    return false;
+}
+
+
+console.log( myIncludes(array, 'Яблоко')); // true
+console.log( myIncludes(array, 'Яблоко', 1)); // true
+console.log( myIncludes(array, false, -3)); // true
+console.log( myIncludes(array, NaN)); // true
+console.log( myIncludes(array, 'Яблоко', -1)); // false
--- tasks/task9/myIndexOf/myIndexOf.js
+++ tasks/task9/myIndexOf/myIndexOf.js
@@ -2,6 +2,7 @@ let array = ['Яблоко', 'Апельсин', 'Яблоко', false];
 
 function myIndexOf(array, item, from = 0) {
     let findedIndex = false;
+    if (from < 0) from = array.length + from;
     for (let i = from; i < array.length; i++) {
         if (array[i] === item) {
             findedIndex = i;
@@ -15,3 +16,4 @@ console.log( myIndexOf(array, 'Яблоко')); // 0
 console.log( myIndexOf(array, 'Яблоко', 1)); // 2
 console.log( myIndexOf(array, 'Яблок2141о')); // -1
 console.log( myIndexOf(array, false)); // 3
+console.log( myIndexOf(array, false, -3)); // 3
