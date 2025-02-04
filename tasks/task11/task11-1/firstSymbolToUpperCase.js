4f4d4b1c8ac10eedc47c6414965044177805df43
Noikstrax <noikstrax@tuta.io>
2025-02-04 14:03:42 +0300

    add task 11 folder and task11-1 script

---
+++ tasks/task11/task11-1/firstSymbolToUpperCase.js
@@ -0,0 +1,7 @@
+function firstSymbolToUpperCase(str) {
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+let str = "hello";
+console.log(firstSymbolToUpperCase(str));
