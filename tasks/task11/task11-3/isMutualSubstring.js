1047a71f1c152d470fa18095c123fc893eeef656
Noikstrax <noikstrax@tuta.io>
2025-02-04 15:11:21 +0300

    Add task 11-3

---
+++ tasks/task11/task11-3/isMutualSubstring.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function isMutualSubstring(str1, str2) {
+    return (str1.includes(str2)) ? true :
+    (str2.includes(str1)) ? true : false;
+}
+
+
+let string1 = 'Дверь машина собака';
+let string2 = 'машина';
+let string3 = 'Дом';
+
+console.log(isMutualSubstring(string1, string2)); // true
+console.log(isMutualSubstring(string1, string3)); // false
